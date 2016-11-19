2943dd68a524b70a828a8e7b5dbabb334efbf6f7
Julia Chen Jing <juliachenjing@gmail.com>
2016-11-18 19:49:26 -0600

    Add files via upload

--- src/main/resources/Customer.java
+++ src/main/resources/Customer.java
@@ -1,4 +1,27 @@
+import java.math.BigDecimal;
+import java.util.Date;
 
-public class Client {
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Customer{
+
+
+	private String customerID;
+	
+
+	private String name;
+	
+	private String address;
 
 }
